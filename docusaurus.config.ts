65c4d469693070375e4c0a6c5751671a403f23d3
KillEdgier <123609015+KillEdgier@users.noreply.github.com>
2023-12-19 21:45:31 -0600

    Updated config.ts to point to GH pages URL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://killedgier.github.io/catstructions/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
