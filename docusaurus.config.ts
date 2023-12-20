3fec8be1c3eb4d7ff6a85db5cbf3c3af9ea20785
Kell Sanders <Kell.Sanders@target.com>
2023-12-20 08:14:39 -0600

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   organizationName: 'KillEdgier', // Usually your GitHub org/user name.
   projectName: 'catstructions', // Usually your repo name.
   deploymentBranch: 'gh-pages'
-  trailingSlash: false,
+  trailingSlash: true,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
