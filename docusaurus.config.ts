10e95400744a3e48d6ec4ab867c9fbe3b312dcd9
Kell Sanders <Kell.Sanders@target.com>
2023-12-21 09:42:46 -0600

    added missing slash to baseURL

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://killedgier.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/catstructions',
+  baseUrl: '/catstructions/',
   // Testing
 
   // GitHub pages deployment config.
