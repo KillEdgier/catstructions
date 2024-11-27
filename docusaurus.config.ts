d961c0678179eb92251ce2b9595aae42c52c9637
Kell Sanders <Kell.Sanders@target.com>
2024-11-27 13:56:13 -0600

    nov updates

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,9 +22,6 @@ const config: Config = {
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
     locales: ['en'],
